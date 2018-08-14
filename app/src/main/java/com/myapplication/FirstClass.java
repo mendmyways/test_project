1fdcb98d6a24857d2ca50f0b1ebbd1fe481423ea
sunlin <2586565381@qq.com>
2018-08-14 10:50:54 +0800

    second commit

---
+++ app/src/main/java/com/myapplication/FirstClass.java
@@ -0,0 +1,17 @@
+package com.myapplication;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class FirstClass extends View{
+
+    public FirstClass(Context context) {
+        super(context);
+    }
+
+    public FirstClass(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
--- app/src/main/java/com/myapplication/MainActivity.java
+++ app/src/main/java/com/myapplication/MainActivity.java
@@ -24,34 +24,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //开始初始化控件
-        /* thread=new Thread(new Runnable() {
-              @Override
-              public void run() {
-                  try {
-                      Thread.sleep(3000);
-
-                      handler.sendEmptyMessage(SEND_FLAG);
-
-                  } catch (InterruptedException e) {
-                      e.printStackTrace();
-                  }
-              }
-          });
-        thread.start();*/
-        /**
-         * 线程的启动与销毁
-         * */
-        //创建线程
-       // thread=new HandlerThread("getTestInfo");
-       /* thread.start(); //启动线程
-        //创建handler
-        mHandler=new Handler(thread.getLooper());//使用HandlerThread的looper对象创建Handler，如果使用默认的构造方法，很有可能阻塞UI线程
-        mHandler.post(mBackgroundRunable);//将线程post到handler中*/
         btnStart= (Button)findViewById(R.id.tvStart);
         btnStart.setOnClickListener(this);
-
-
 
     }
     Handler uiHandler=new Handler(){
@@ -99,8 +73,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onDestroy() {
         super.onDestroy();
         mHandler.removeCallbacks(mBackgroundRunable);
-        Log.i("info","thread has destroied");
-        Toast.makeText(this,"close_thread",Toast.LENGTH_LONG).show();
+
     }
 
 
